fb36b874c21ceac572c0238a9a3514d85b24e117
diogo <diogo464@protonmail.com>
2020-07-24 17:35:03 +0100

    Implemented 'Display' and 'std::error::Error' for gma error type

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,6 @@
 use crate::binary;
+use lzma_rs;
+use std::fmt::Display;
 
 #[derive(Debug)]
 pub enum Error {
@@ -7,6 +9,7 @@ pub enum Error {
     UTF8Error(std::string::FromUtf8Error),
     InvalidIdent,
     InvalidVersion(u8),
+    CompressionError(lzma_rs::error::Error),
 }
 
 impl From<std::io::Error> for Error {
@@ -30,3 +33,18 @@ impl From<binary::Error> for Error {
         }
     }
 }
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::IOError(e) => e.fmt(f),
+            Self::InvalidString => write!(f, "An invalid string was found, strings cant have the null byte and should only contain ascii characters"),
+            Self::UTF8Error(e) => e.fmt(f),
+            Self::InvalidIdent => write!(f, "The gma file did not containt a valid ident, 'GMAD' was expect at the start of the file"),
+            Self::InvalidVersion(v) => write!(f, "An invalid version of gma file was found : '{}', this might be cause by a corrupt file", v),
+            Self::CompressionError(e) => write!(f, "Error while compressing/decompressing. {:?}", e),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
